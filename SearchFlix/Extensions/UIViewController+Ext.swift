d29969d7dbfb27ab117e3690327c3f8f11fcc9de
MineRala <mine-rala@hotmail.com>
2025-02-20 18:30:06 +0300

    Alert main thread control added.

--- SearchFlix/Extensions/UIViewController+Ext.swift
+++ SearchFlix/Extensions/UIViewController+Ext.swift
@@ -7,20 +7,18 @@
 
 import UIKit
 
-import UIKit
-
 extension UIViewController {
-
     func showAlert(title: String = "Error", message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-
-        // Ok butonu ekliyoruz
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(okAction)
 
-        // Alert'i ekranda gösteriyoruz
-        DispatchQueue.main.async {
+        if Thread.isMainThread {
             self.present(alertController, animated: true, completion: nil)
+        } else {
+            DispatchQueue.main.async {
+                self.present(alertController, animated: true, completion: nil)
+            }
         }
     }
 }
